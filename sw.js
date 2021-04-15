f726b01d22b1ef357ebb72d84ceadd4b97c967c8
Jeroen van Tilburg <jvantilburg@gmail.com>
2021-04-15 12:04:29 +0200

    Added service worker

---
+++ sw.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'motionticker-cache-v1';
+var urlsToCache = [
+  '/motionticker/',
+  'index.html',
+  'style.css',
+  'scripts/MediaInfo.js',
+  'scripts/MediaInfoWasm.wasm',
+  'scripts/motionticker.js',
+  'scripts/MediaInfo.js.mem',
+  'scripts/fabric-patch-arrow.js',
+  'scripts/registerSW.js',
+  'scripts/MediaInfoWasm.js',
+  'scripts/fabric-patch-touch.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.0/papaparse.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.3.1/fabric.min.js',
+  'https://docs.opencv.org/4.5.1/opencv.js',
+  'videos/demo_bounching_ball.mp4',
+  'img/screenshot.png',
+  'apple-touch-icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+  'mstile-70x70.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
