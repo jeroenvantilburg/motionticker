08b03e91389ab04d8e61c777a293d13771210360
Jeroen van Tilburg <jvantilburg@gmail.com>
2022-12-30 20:33:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,12 +22,13 @@ var urlsToCache = [
   'scripts/opencv.js',
   //'videos/demo_bounching_ball.mp4',
   'apple-touch-icon.png',
+  'android-chrome-192x192.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.eot',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.svg',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
   'mstile-70x70.png'
 ];
 
